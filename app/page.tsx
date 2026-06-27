d2479eb12f0cda00193bee82c0add631401d6fd8
sora-0311-1131 <2675048x@stu.kobe-u.ac.jp>
2026-06-27 16:45:10 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { connection } from "next/server";
-import { CatImage } from "./cat-image";
-import { fetchImage } from "./fetch-image";
-
-export default async function Home() {
-  await connection();
-  const image = await fetchImage();
-  return <CatImage url={image.url} />;
-}
